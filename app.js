39b0a5c87ec5532dc52de0c1063e60837dd75062
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-07-17 21:00:51 +0530

    Scrolll

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const socketIo = require("socket.io");
 const http = require("http");
+const path = require("path")
 
 const server = http.createServer(app)
 const io = socketIo(server);
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
